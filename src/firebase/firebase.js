228170403e9557795295011c9e0245fa9fe12dbf
Anant Gaire <56.anant@gmail.com>
2021-02-20 07:14:27 +0545

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA3WZOqZ5hq3zSfkYhZnCABgID_Y2mPzgk',
+  authDomain: 'slack-clone-e8d7f.firebaseapp.com',
+  projectId: 'slack-clone-e8d7f',
+  storageBucket: 'slack-clone-e8d7f.appspot.com',
+  messagingSenderId: '140581541855',
+  appId: '1:140581541855:web:710b64a911d296b1cc566a',
+};
